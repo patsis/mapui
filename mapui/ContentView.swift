4cac647a037f4b0f09fa21fe9d0bf578cecaa796
Harry Patsis <patsis@gmail.com>
2019-11-29 00:03:12 +0200

    added search

--- mapui/ContentView.swift
+++ mapui/ContentView.swift
@@ -9,61 +9,110 @@
 import SwiftUI
 import MapKit
 
+
+//class MapPin {
+//  let coordinate: CLLocationCoordinate2D
+//  let title: String?
+//  let subtitle: String?
+//  init(title: String, subtitle: String, coordinate: CLLocationCoordinate2D) {
+//    self.title = title
+//    self.subtitle = subtitle
+//    self.coordinate = coordinate
+//  }
+//}
+
 struct MapView: UIViewRepresentable {
+//  @State var pin: MapPin? = nil
+  @Binding var pin: MKPlacemark?
   func makeUIView(context: Context) -> MKMapView {
     MKMapView(frame: .zero)
   }
-  //48.212423, 16.374150
-
+  
   func updateUIView(_ uiView: MKMapView, context: UIViewRepresentableContext<MapView>) {
-    let coordinate = CLLocationCoordinate2D(latitude: 48.2082, longitude: 16.3738)
-    let span = MKCoordinateSpan(latitudeDelta: 8.0, longitudeDelta: 8.0)
-    let region = MKCoordinateRegion(center: coordinate, span: span)
+    guard let pin = pin else {
+//    if pins.count == 0 {
+      let coordinate = CLLocationCoordinate2D(latitude: 48.2082, longitude: 16.3738)
+      let span = MKCoordinateSpan(latitudeDelta: 8.0, longitudeDelta: 8.0)
+      let region = MKCoordinateRegion(center: coordinate, span: span)
+      uiView.setRegion(region, animated: true)
+      return
+    }
+    // 4.
+    let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+    let region = MKCoordinateRegion(center: pin.coordinate, span: span)
     uiView.setRegion(region, animated: true)
+    
+    // 5.
+//    let annotation = MKPointAnnotation()
+//    annotation.coordinate = pin.coordinate
+//    annotation.title = pin.title
+//    annotation.subtitle = pin.subtitle
+    uiView.addAnnotation(pin)
   }
 }
 
 extension UIApplication {
-    func endEditing(_ force: Bool) {
-        self.windows
-            .filter{$0.isKeyWindow}
-            .first?
-            .endEditing(force)
-    }
+  func endEditing(_ force: Bool) {
+    self.windows
+      .filter{$0.isKeyWindow}
+      .first?
+      .endEditing(force)
+  }
 }
 
 struct ContentView: View {
   @State var name: String = ""
   @State var showCancelButton: Bool = false
+  @State var mapItems: [MKMapItem] = []
+  @State var pin: MKPlacemark? = nil
+  
+  func searchPlaces(name: String) {
+    let searchRequest = MKLocalSearch.Request()
+    searchRequest.naturalLanguageQuery = name
+    let search = MKLocalSearch(request: searchRequest)
+    search.start {response, error in
+      guard let response = response else {
+        self.mapItems = []
+        return
+      }
+      self.mapItems = response.mapItems
+    }
+  }
+  
+  func setLocation(place: MKPlacemark) {
+    self.mapItems = []
+    pin = place
+  }
+  
   var body: some View {
     ZStack {
-      MapView()
+      MapView(pin: $pin)
       VStack {
         HStack {
           HStack {
             if showCancelButton {
-            Button(action: {
-              print("button pressed")
-              UIApplication.shared.endEditing(true) // this must be placed before the other commands here
-              self.name = ""
-              self.showCancelButton = false
-            }) {
-              Image(systemName: "chevron.left")
-                .renderingMode(.original)
+              Button(action: {
+                print("button pressed")
+                UIApplication.shared.endEditing(true) // this must be placed before the other commands here
+                self.name = ""
+                self.showCancelButton = false
+                self.mapItems = []
+              }) {
+                Image(systemName: "chevron.left")
+                  .renderingMode(.original)
               }
             } else {
               Image(systemName: "magnifyingglass")
             }
             
             TextField("search...", text: $name, onEditingChanged: { isEditing in
-              print(isEditing)
               if isEditing {
                 self.showCancelButton = true
               } else {
                 self.showCancelButton = false
               }
             }, onCommit: {
-              print("onCommit")
+              self.searchPlaces(name: self.name)
             }).textFieldStyle(PlainTextFieldStyle())
             
             Button(action: {
@@ -72,23 +121,32 @@ struct ContentView: View {
             {
               Image(systemName: "xmark").opacity(name == "" ? 0 : 1)
             }
-            
-            //            if showCancelButton  {
-            //              Button("Cancel") {
-            //                //              UIApplication.shared.endEditing(true) // this must be placed before the other commands here
-            //                self.name = ""
-            //                self.showCancelButton = false
-            //              }
-            //              .foregroundColor(Color(.systemBlue))
-            //            }
-            
           }
           .padding(EdgeInsets(top: 6, leading: 12, bottom: 6, trailing: 12))
           .background(Color.white)
           .cornerRadius(50)
           .padding()
-          
-          
+        }
+
+
+        
+        if (mapItems.count > 0) {
+          List(mapItems, id: \.self) { item in
+            if item.placemark.name != nil {
+              Text(item.placemark.name!)
+                .frame(height: 20)
+                .background(Color.white)
+                .gesture(TapGesture()
+                  .onEnded {
+                    self.setLocation(place: item.placemark)
+                  }
+              )
+            }
+          }
+          .cornerRadius(8)
+          .padding()
+          .frame(height: 165)
+
         }
         Spacer()
       }
